4dca571fa1555df0841b729b7d3bd26c29e040a0
Zach Walsh <zachwalsh2219@live.com>
2024-01-16 23:41:34 -0700

    update logic

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -16,48 +16,44 @@ func _process(delta):
 func doDamage(tilePos: Vector2i):
 	var tile: TileData = tileMap.get_cell_tile_data(0, tilePos)
 	var health = tile.get_custom_data("health")
+	var nextHealth =  health - 1
 	var type   = tile.get_custom_data("type")
 	var status = tile.get_custom_data("status")
+	var burnable = tile.get_custom_data("burnable")
 
-	if health >= 0 && status != 2:
-		tileMap.set_cell(0, tilePos, 0, Vector2i(status, type), health)
-		if (health == 0):
-			changeStatus(tilePos)
-
-		return true
-	else:
-		return false
-
-func changeStatus(tilePos: Vector2i):
-	var tile: TileData = tileMap.get_cell_tile_data(0, tilePos)
-	var type   = tile.get_custom_data("type")
-	var status = tile.get_custom_data("status")
-	tileMap.set_cell(0, tilePos, 0, Vector2i(status, type))
-	if status >= 2:
-		tilesOnFire.erase(tilePos)
+	if nextHealth >= 0 && status < 2:
+		if (nextHealth== 0):
+			tileMap.set_cell(0, tilePos, 0, Vector2i(status+1, type), nextHealth)
+		else:
+			tileMap.set_cell(0, tilePos, 0, Vector2i(status, type), health)
 
 
 func _on_timer_timeout():
 
 	var futureTilesOnFire = []
+	var tilesToErase = []
 
 	for t in tilesOnFire:
 		var currentTile: TileData = tileMap.get_cell_tile_data(0, t)
 
 		if !currentTile:
 			continue
-		
-		doDamage(t)
 
 		var neighbors : Array[Vector2i] = tileMap.get_surrounding_cells(t)
 
 		for n in neighbors:
 			var neighborTile: TileData = tileMap.get_cell_tile_data(0, n)
-
-			if !neighborTile || neighborTile.get_custom_data("status") >= 2:
+			if !neighborTile || !neighborTile.get_custom_data("burnable"):
 				continue
 
-			if doDamage(n):
+			doDamage(n)
+			if tileMap.get_cell_tile_data(0,n).get_custom_data("onFire"):
 				futureTilesOnFire.append(n)
+				
+		doDamage(t)
+		if !tileMap.get_cell_tile_data(0,t).get_custom_data("onFire"):
+			tilesToErase.append(t)
 
 	tilesOnFire = tilesOnFire + futureTilesOnFire
+	for e in tilesToErase:
+		tilesOnFire.erase(e)
