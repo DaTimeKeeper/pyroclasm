9f5a83d2f19cd325d6a7798f6b2cc9c8d374c951
Alex <Alexwalsh.biz@gmail.com>
2024-01-16 18:17:56 -0700

    fire spread update

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -8,39 +8,56 @@ enum TILE_TYPES  {GRASS, BUSH, TRESS}
 enum TILE_STATUS {GREEN, BURNING, BURNED, WET}
 
 var startingFireTile = Vector2i(1,1)
-var tilesOnFire      = [startingFireTile]
+var tilesOnFire  = [startingFireTile]
 
 func _process(delta):
 	pass
 		
-func doDamage(tilePosition:Vector2i):
-	var tile : TileData = tileMap.get_cell_tile_data(0, tilePosition)
-	var health 			= tile.get_custom_data("health")
-	var type   			= tile.get_custom_data("type")
+func doDamage(tilePos: Vector2i):
+	var tile: TileData = tileMap.get_cell_tile_data(0, tilePos)
+	var health = tile.get_custom_data("health")
+	var type   = tile.get_custom_data("type")
+	var status = tile.get_custom_data("status")
 
-	tileMap.set_cell(0, tilePosition, 0, Vector2i(0, type), health)
+	if health >= 0 && status != 2:
+		tileMap.set_cell(0, tilePos, 0, Vector2i(status, type), health)
+		if (health == 0):
+			changeStatus(tilePos)
 
-	if (health - 1) == 0:
-		return tilePosition
+		return true
+	else:
+		return false
+
+func changeStatus(tilePos: Vector2i):
+	var tile: TileData = tileMap.get_cell_tile_data(0, tilePos)
+	var type   = tile.get_custom_data("type")
+	var status = tile.get_custom_data("status")
+	tileMap.set_cell(0, tilePos, 0, Vector2i(status, type))
+	if status >= 2:
+		tilesOnFire.erase(tilePos)
+
 
 func _on_timer_timeout():
 
 	var futureTilesOnFire = []
 
 	for t in tilesOnFire:
-		var currentTile = tileMap.get_cell_tile_data(0, t)
+		var currentTile: TileData = tileMap.get_cell_tile_data(0, t)
 
 		if !currentTile:
 			continue
+		
+		doDamage(t)
 
 		var neighbors : Array[Vector2i] = tileMap.get_surrounding_cells(t)
 
 		for n in neighbors:
-			var neighborTile = tileMap.get_cell_tile_data(0, n)
+			var neighborTile: TileData = tileMap.get_cell_tile_data(0, n)
 
-			if !neighborTile:
+			if !neighborTile || neighborTile.get_custom_data("status") >= 2:
 				continue
-			
-				futureTilesOnFire.append(doDamage(n))
+
+			if doDamage(n):
+				futureTilesOnFire.append(n)
 
 	tilesOnFire = tilesOnFire + futureTilesOnFire
