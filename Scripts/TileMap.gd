b15b1929866a7ca96da84b38318d2b154a47623b
Zach Walsh <zachwalsh2219@live.com>
2024-01-21 23:31:25 -0700

    make changes

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -2,59 +2,68 @@ extends TileMap
 
 @onready var tileMap: TileMap = $"."
 
-signal addScorce(points: int)
+signal setScorce(points: int)
 signal allFireOut(finalScore: int)
+var startingFireTile = Vector2i(0,0)
+var tilesOnFire      = [startingFireTile]
+var score = 0
 
 func _on_update_fire_timer_timeout():
-
-	var tilesOnFire: Array[Vector2i] = getTilesOnFire()
+	var futureTilesOnFire = []
+	var tilesToErase = []
+	
+	if tilesOnFire.size()==0: #Check End state
+		allFireOut.emit(score)
 
 	for t in tilesOnFire:
-		if tileMap.get_cell_tile_data(0, t) != null:
-			burnNeigbors(t)
-			doDamage(t)
-			if tileMap.get_cell_tile_data(0, t).get_custom_data("status") == 2:
-				tileMap.erase_cell(1, t)
+		var currentTile: TileData = tileMap.get_cell_tile_data(0, t)
+
+		if !currentTile:
+			continue
+
+		var neighbors : Array[Vector2i] = tileMap.get_surrounding_cells(t)
+
+		for n in neighbors:
+			var neighborTile: TileData = tileMap.get_cell_tile_data(0, n)
+			if !neighborTile || !neighborTile.get_custom_data("burnable")||checkOnFire(n):
+				continue
+
+			doDamage(n)
+			if checkOnFire(n):
+				futureTilesOnFire.append(n)
+				
+		doDamage(t)
+		if !checkOnFire(t):
+			tilesToErase.append(t)
+
+	tilesOnFire = tilesOnFire + futureTilesOnFire
+	for e in tilesToErase:
+		tilesOnFire.erase(e)
+		score+=1
 		
-	addScorce.emit(countBurnedTiles()) #update Score
+	setScorce.emit(score)
 
 func doDamage(tilePos: Vector2i):
-
 	var tile: TileData = tileMap.get_cell_tile_data(0, tilePos)
-	var nextHealth     = tile.get_custom_data("health") - 1
-	var type           = tile.get_custom_data("type")
-	var status         = tile.get_custom_data("status")
+	var nextHealth =  tile.get_custom_data("health") - 1
+	var type   = tile.get_custom_data("type")
+	var status = tile.get_custom_data("status")
 
-	if status < 2:
-		if (nextHealth <= 0):
-			tileMap.set_cell(0, tilePos, 0, Vector2i(status + 1, type))
-			if status + 1 == 1:
+	#TDOD: add water overshield check
+	# If waterShield >0 damage that then continue else run below
+
+	if nextHealth >= 0 && status < 2:
+		if (nextHealth<= 0):
+			tileMap.set_cell(0, tilePos, 0, Vector2i(status+1, type))
+			if status+1 == 1 && !checkOnFire(tilePos): 
 				setOnfire(tilePos)
 		else:
 			tileMap.set_cell(0, tilePos, 0, Vector2i(status, type), nextHealth)
-
+			if status == 1 && !checkOnFire(tilePos): 
+				setOnfire(tilePos)
 	
 
-func countBurnedTiles():
-
-	var burnTiles: Array[Vector2i] = []
-
-	for x in range(3):
-		burnTiles += tileMap.get_used_cells_by_id(0, 0, Vector2i(2, x)) 
-
-	return burnTiles.size()
-
-func getTilesOnFire():
-
-	var tilesburning : Array[Vector2i] = tileMap.get_used_cells_by_id(1, 1, Vector2i(0, 0))
-
-	if !tilesburning:
-		allFireOut.emit(countBurnedTiles())
-		pass
-	
-	return tilesburning
-
-func checkOnfire(tilePos: Vector2i):
+func checkOnFire(tilePos: Vector2i):
 
 	if(tileMap.get_cell_tile_data(1, tilePos)):
 		return true
@@ -64,19 +73,3 @@ func checkOnfire(tilePos: Vector2i):
 func setOnfire(tilePos: Vector2i):
 
 	tileMap.set_cell(1, tilePos, 1, Vector2i(0, 0))
-
-func burnNeigbors(tilePos: Vector2i):
-
-	var neighbors : Array[Vector2i] = tileMap.get_surrounding_cells(tilePos)
-
-	for n in neighbors:
-
-		var neighborTile: TileData = tileMap.get_cell_tile_data(0, n)
-		if neighborTile != null: # TODO: add check if tile is on fire
-			doDamage(n)
-				
-		
-
-		
-
-
