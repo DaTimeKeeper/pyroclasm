f4fb48bb62d42df75489be9b1132ae4d6faebe2c
Alex <Alexwalsh.biz@gmail.com>
2024-01-22 21:53:27 -0700

    control fire with wind

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -16,6 +16,24 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("pause") && !gameOver:
 		setPause()
+	if Input.is_action_just_pressed("wind_north"):
+		$"PlayerCamera2/wind/Arrow".rotation_degrees = 0
+		$"PlayerCamera2/wind/WindText".text = "N"
+		$"TileGameMap".windDir = Vector2i(-1,-1)
+	if Input.is_action_just_pressed("wind_west"):
+		$"PlayerCamera2/wind/Arrow".rotation_degrees = 90
+		$"PlayerCamera2/wind/WindText".text = "W"
+		$"TileGameMap".windDir = Vector2i(1,-1)
+	if Input.is_action_just_pressed("wind_south"):
+		$"PlayerCamera2/wind/Arrow".rotation_degrees = 180
+		$"PlayerCamera2/wind/WindText".text = "S"
+		$"TileGameMap".windDir = Vector2i(1, 1)
+	if Input.is_action_just_pressed("wind_east"):
+		$"PlayerCamera2/wind/Arrow".rotation_degrees = -90
+		$"PlayerCamera2/wind/WindText".text = "E"
+		$"TileGameMap".windDir = Vector2i(-1,1)
+
+
 
 func setPause():
 	if paused:
@@ -24,7 +42,7 @@ func setPause():
 	else:
 		pause_menu.show()
 		Engine.time_scale = 0
-	
+
 	paused = !paused
 
 
--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -7,11 +7,12 @@ signal allFireOut(finalScore: int)
 var startingFireTile = Vector2i(0,0)
 var tilesOnFire      = [startingFireTile]
 var score = 0
+var windDir: Vector2i = Vector2i(0,0)
 
 func _on_update_fire_timer_timeout():
 	var futureTilesOnFire = []
 	var tilesToErase = []
-	
+
 	if tilesOnFire.size() == 0: #Check End state
 		allFireOut.emit(score)
 
@@ -21,29 +22,40 @@ func _on_update_fire_timer_timeout():
 		if !currentTile:
 			continue
 
-		var neighbors: Array[Vector2i] = tileMap.get_surrounding_cells(t)
+		var neighbors: Array[Vector2i] = getAllAround(t)
 
 		for n in neighbors:
 			var neighborTile: TileData = tileMap.get_cell_tile_data(0, n)
+
 			if !neighborTile || !neighborTile.get_custom_data("burnable") ||checkOnFire(n):
 				continue
 
-			doDamage(n)
+
+			if t + windDir == n:
+				doDamage(n)
+				doDamage(n)
+				#doDamage(n)
+			elif t + Vector2i(windDir.x, 0) == n || t + Vector2i(0, windDir.y) == n:
+				doDamage(n)
+				#doDamage(n)
+			#elif (t + Vector2i(windDir.x, 0)) - Vector2i(0, windDir.y) == n || (t + Vector2i(0, windDir.y)) - Vector2i(windDir.x, 0) == n:
+				#doDamage(n)
+
 			if checkOnFire(n):
 				futureTilesOnFire.append(n)
-				
-				
+
+
 		doDamage(t)
 		if checkOnFire(t) && tileMap.get_cell_tile_data(0, t).get_custom_data("status") == 2:
 			tilesToErase.append(t)
-			
+
 
 	tilesOnFire = tilesOnFire + futureTilesOnFire
 	for e in tilesToErase:
 		tilesOnFire.erase(e)
 		tileMap.erase_cell(1,e)
 		score += 1
-		
+
 	setScorce.emit(score)
 
 func doDamage(tilePos: Vector2i):
@@ -58,13 +70,13 @@ func doDamage(tilePos: Vector2i):
 	if nextHealth >= 0 && status < 2:
 		if (nextHealth <= 0):
 			tileMap.set_cell(0, tilePos, 0, Vector2i(status+1, type))
-			if (status + 1 == 1 || status == 1) && !checkOnFire(tilePos): 
+			if (status + 1 == 1 || status == 1) && !checkOnFire(tilePos):
 				setOnfire(tilePos)
 		else:
 			tileMap.set_cell(0, tilePos, 0, Vector2i(status, type), nextHealth)
-			if status == 1 && !checkOnFire(tilePos): 
+			if status == 1 && !checkOnFire(tilePos):
 				setOnfire(tilePos)
-	
+
 
 func checkOnFire(tilePos: Vector2i):
 
@@ -76,3 +88,14 @@ func checkOnFire(tilePos: Vector2i):
 func setOnfire(tilePos: Vector2i):
 
 	tileMap.set_cell(1, tilePos, 1, Vector2i(0, 0))
+
+func getAllAround(tilePos: Vector2i):
+	var listTiles: Array[Vector2i] = []
+	var dir: Array[Vector2i] = [Vector2i(0,1),  Vector2i(0,-1),
+								Vector2i(1,0),  Vector2i(-1, 0),
+								Vector2i(1,1),  Vector2i(-1,-1),
+								Vector2i(-1,1), Vector2i(1,-1)]
+	for i in dir:
+		listTiles.append(tilePos + i)
+
+	return listTiles
